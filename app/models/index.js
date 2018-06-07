e098eb6ec45a1fa410e843977051d0ab5d8c2139
suarezluis <suarezluis@gmail.com>
2018-06-07 10:30:12 -0500

    heroku is being a bitch

--- app/models/index.js
+++ app/models/index.js
@@ -1,10 +1,12 @@
-"use strict";
-
-var fs = require("fs");
-var path = require("path");
-var Sequelize = require("sequelize");
-var env = process.env.NODE_ENV || "production";
-var config = require(__dirname + "/../config/config.json")[env];
+'use strict';
+//require dependencies
+var fs = require('fs');
+var path = require('path');
+var Sequelize = require('sequelize');
+var basename = path.basename(module.filename);
+var env = process.env.NODE_ENV || 'production';
+var config = require(__dirname + '/../config/config.json')[env];
+var db = {};
 
 //configuring the env with sequelize
 if (config.use_env_variable) {
@@ -13,24 +15,23 @@ if (config.use_env_variable) {
 	var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-var db = {};
-
-fs.readdirSync(__dirname)
-  .filter(function(file) {
-    return file.indexOf(".") !== 0 && file !== "index.js";
-  })
-  .forEach(function(file) {
-    var model = sequelize.import(path.join(__dirname, file));
-    db[model.name] = model;
-  });
+fs
+	.readdirSync(__dirname)
+	.filter(function(file) {
+		return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+	})
+	.forEach(function(file) {
+		var model = sequelize['import'](path.join(__dirname, file));
+		db[model.name] = model;
+	});
 
 Object.keys(db).forEach(function(modelName) {
-  if ("associate" in db[modelName]) {
-    db[modelName].associate(db);
-  }
+	if (db[modelName].associate) {
+		db[modelName].associate(db);
+	}
 });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+module.exports = db
